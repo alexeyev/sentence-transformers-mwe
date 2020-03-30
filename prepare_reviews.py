355ebe960ce05ab6e877ac6530e1ecb2978aa364
alexeyev <anton.m.alexeyev@gmail.com>
2020-03-30 17:44:14 +0300

    [ENH] Reviews sentence embeddings

--- prepare_reviews.py
+++ prepare_reviews.py
@@ -8,17 +8,20 @@ nltk.download('punkt')
 model = SentenceTransformer("./bert-base-nli-mean-tokens")
 FILEPATH = "food_reviews.txt"
 
-with open(FILEPATH, "r+") as rf:
+with open(FILEPATH, "r+", errors='ignore') as rf:
     with open(FILEPATH + ".reviews", "w+") as wtext:
         with open(FILEPATH + ".vectors", "w+") as wvectors:
 
             for line in tqdm(rf, "lines of " + FILEPATH):
-                sentences = sent_tokenize(line, language="english")
-                sentence_embeddings = model.encode(sentences)
+                try:
+                    sentences = sent_tokenize(line, language="english")
+                    sentence_embeddings = model.encode(sentences)
 
-                for sentence, emb in zip(sentences, sentence_embeddings):
-                    wtext.write(sentence.strip() + "\n")
-                    wvectors.write(" ".join([str(v) for v in list(emb)]) + "\n")
+                    for sentence, emb in zip(sentences, sentence_embeddings):
+                        wtext.write(sentence.strip() + "\n")
+                        wvectors.write(" ".join([str(v) for v in list(emb)]) + "\n")
 
-                wtext.write("\n")
-                wvectors.write("\n")
+                    wtext.write("\n")
+                    wvectors.write("\n")
+                except Exception as e:
+                    print("Error:", e)
