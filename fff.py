ae203e710f8a308ab2abba18b708d9e9357a292a
aam <aam@artin.pdmi.ras.ru>
2020-04-03 11:27:13 +0300

    more stuff

---
+++ fff.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+import pandas as pd
+
+df = pd.read_csv("/media/data/datasets/yelp/food/ethnic_food_top10categories-sentences-vectors.tsv", sep="\t")
+
+model = SentenceTransformer("bert-base-nli-mean-tokens")  
+df["embeddings"] = model.encode(df.text.str.lower().tolist(), batch_size=512, show_progress_bar=True)
+df["embeddings"] = df.embeddings.map(lambda x: " ".join([str(f) for f in list(x)]))
+
+df.to_csv("\media\data\datasets\yelp\food\that_thing.tsv", sep="\t")
