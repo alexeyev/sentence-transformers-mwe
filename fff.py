c84a018c9afa91399f8818b78fd186c530bbc907
alexeyev <anton.m.alexeyev@gmail.com>
2020-04-03 11:43:18 +0300

    [ENH] limiting vector file size

--- fff.py
+++ fff.py
@@ -2,10 +2,10 @@ from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 import pandas as pd
 
-df = pd.read_csv("/media/data/datasets/yelp/food/ethnic_food_top10categories-sentences-vectors.tsv", sep="\t").head(5000)
+df = pd.read_csv("/media/data/datasets/yelp/food/ethnic_food_top10categories-sentences-vectors.tsv", sep="\t")#.head(5000)
 
 model = SentenceTransformer("bert-base-nli-mean-tokens")  
 df["embeddings"] = model.encode(df.text.str.lower().tolist(), batch_size=512, show_progress_bar=True)
 df["embeddings"] = df.embeddings.map(lambda x: " ".join([str(f) for f in list(x)]))
 
-df.to_csv("\media\data\datasets\yelp\food\that_thing.tsv", sep="\t")
+df.to_csv("/media/data/datasets/yelp/food/that_thing.tsv", sep="\t")
