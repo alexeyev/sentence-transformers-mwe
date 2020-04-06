184869a1257a70684b6b6a5716e6b23e6c59490a
alexeyev <anton.m.alexeyev@gmail.com>
2020-04-06 03:45:15 +0300

    [ENH] Minor changes

--- 02_to_vectors.py
+++ 02_to_vectors.py
@@ -2,10 +2,12 @@ from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 import pandas as pd
 
-df = pd.read_csv("/media/data/datasets/yelp/food/ethnic_food_top10categories-sentences-vectors.tsv", sep="\t")#.head(5000)
+df = pd.read_csv("/media/data/datasets/yelp/food/ethnic_food_top10categories-splitted.csv").head(100000)
+
+print(df.text.tolist()[:10])
 
 model = SentenceTransformer("bert-base-nli-mean-tokens")  
-df["embeddings"] = model.encode(df.text.str.lower().tolist(), batch_size=512, show_progress_bar=True)
+df["embeddings"] = model.encode(df.text.tolist(), batch_size=512, show_progress_bar=True)
 df["embeddings"] = df.embeddings.map(lambda x: " ".join([str(f) for f in list(x)]))
 
-df.to_csv("/media/data/datasets/yelp/food/that_thing.tsv", sep="\t")
+df.to_csv("/media/data/datasets/yelp/food/ethnic_food_top10categories-vectorized.tsv", sep="\t")
